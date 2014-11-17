4a30ea8b210d4a85d281515328f322ec589ef15b
Greg McCarvell <greg@gregmccarvell.com>
2014-11-17 10:48:18 -0500

    Update #3

---
+++ Prototype/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,28 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var documentEvent = {};	// @document
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
+	{// @endlock
+		//WAF.widget.Grid.prototype.testFunction = function(){}; //works with / without component
+		
+		$$('component1').loadComponent({
+			path:'/test.waComponent',
+			onSuccess: function(e){
+				WAF.widget.Grid.prototype.testFunction = function(){};
+			}
+}); //works here
+		
+		//put a grid in the web component breaks it
+
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
+// @endregion
+};// @endlock
